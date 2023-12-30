3c8144d73fee95b52b68ad4644d59b35069ac349
Thu Dang <>
2023-12-30 13:26:55 -0600

    add width and height for the darktheme icon image

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,8 +7,12 @@ import darkMoon from "../../public/images/lightMoon.svg";
 import { useToggle } from "@/hooks/useToggle";
 
 const NavBar = () => {
-  const [isDark, setIsDark] = useToggle(typeof window !== "undefined" ? localStorage.getItem("theme-color") : false);
-  
+  const [isDark, toggleTheme] = useToggle(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("theme-color") === "dark";
+    }
+  });
+
   useEffect(() => {
     document.documentElement.className = isDark ? "dark" : "";
     localStorage.setItem("theme-color", isDark ? "dark" : "light");
@@ -18,8 +22,8 @@ const NavBar = () => {
     <header className="bg-white dark:bg-darkModeInputBg box-shadow">
       <div className="px-4 py-[30px] flex justify-between items-center mx-auto max-w-[1280px] dark:text-white md:px-10">
         <h1 className="text-[14px] font-extrabold leading-[20px] xl:text-[24px] xl:leading-auto">Fun with flags</h1>
-        <div className="flex justify-end items-center cursor-pointer" onClick={setIsDark}>
-          <Image src={isDark ? darkMoon : lightMoon} alt="dark mode switch" className="w-4 h-full object-contain object-center xl:w-5" />
+        <div className="flex justify-end items-center cursor-pointer" onClick={toggleTheme}>
+          <Image src={isDark ? darkMoon : lightMoon} width={16} height={16} alt="dark mode switch" className="w-4 h-full object-contain object-center xl:w-5" />
           <p className="text-[12px] ml-2 font-semibold xl:text-[16px]">Dark Mode</p>
         </div>
       </div>
